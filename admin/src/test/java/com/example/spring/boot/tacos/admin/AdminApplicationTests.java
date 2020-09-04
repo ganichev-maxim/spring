50f14d1afc9a17656aa17aab99b4acc44f88c361
ganichev-maxim <ganichev_maxim@mail.ru>
2020-09-04 23:05:52 +0300

    17 Admin server

---
+++ admin/src/test/java/com/example/spring/boot/tacos/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.tacos.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
